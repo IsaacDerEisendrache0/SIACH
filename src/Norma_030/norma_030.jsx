37d7fe197a9eb81b7421b27fcac1e2c5272819b5
JuanUtch123 <156106033+JuanUtch123@users.noreply.github.com>
2024-09-12 11:58:40 -0700

    tabla modificadas norma30

--- src/Norma_030/norma_030.jsx
+++ src/Norma_030/norma_030.jsx
@@ -1,74 +1,214 @@
-import React from 'react';
-import './Table30.css'; // Asegúrate de crear este archivo CSS con el estilo que te daré
+import React from "react";
+import "./Table30.css";
 
-const SafetyTable = () => {
+const TableComponent = () => {
   return (
     <div className="table-container">
-      <table className="safety-table">
+      <table className="styled-table">
         <thead>
           <tr>
-            <td colSpan="5" className="main-header">
-              <div className="header-logo">
-                <img src="ruta_logo_izquierda" alt="Logo izquierda" />
-              </div>
-              <div className="header-title">
-                AMERICAN INDUSTRIES, S.A. DE C.V. (PROYECTO XINLIDA)
-                <br />
-                <span className="sub-header">
-                  DIRECCIÓN: AV. LIBRE COMERCIO No. 21641 INT. 3 PARQUE INDUSTRIAL AMERICAS CP. 32956
-                </span>
-              </div>
-              <div className="header-logo">
-                <img src="ruta_logo_derecha" alt="Logo derecha" />
-              </div>
-            </td>
+            <th colSpan="7" className="company-header">
+              AMERICAN INDUSTRIES, S.A. DE C.V. (PROYECTO XINLIDA)
+            </th>
           </tr>
           <tr>
-            <td colSpan="5" className="sub-header">
-              6.1 DIAGNÓSTICO INTEGRAL O POR ÁREA DE TRABAJO SOBRE LAS CONDICIONES DE SEGURIDAD Y SALUD EN EL CENTRO DE TRABAJO
-            </td>
+            <th colSpan="7" className="subtitle-header">
+              6.1 DIAGNÓSTICO INTEGRAL POR ÁREA DE TRABAJO SOBRE LAS CONDICIONES DE SEGURIDAD Y SALUD EN EL CENTRO DE TRABAJO
+            </th>
           </tr>
           <tr>
-            <td className="area-header" rowSpan="2">ÁREA</td>
-            <td className="condition-header" rowSpan="2">
+            <th rowSpan="29" className="vertical-header">
+              MATERIA PRIMA
+            </th>
+            <th colSpan="3">
               CONDICIONES FÍSICAS PELIGROSAS O INSEGURAS QUE PUEDAN REPRESENTAR UN RIESGO EN LAS INSTALACIONES, PROCESOS, MAQUINARIA, EQUIPO, HERRAMIENTAS, MEDIOS DE TRANSPORTE, MATERIALES Y ENERGÍA.
-            </td>
-            <td colSpan="2" className="apply-header">APLICA</td>
-            <td className="agent-header" rowSpan="2">
-              AGENTES FÍSICOS, QUÍMICOS Y BIOLÓGICOS CAPACES DE MODIFICAR LAS CONDICIONES DEL MEDIO AMBIENTE EN EL CENTRO DE TRABAJO O QUE, POR SUS PROPIEDADES, CONCENTRACIÓN, NIVEL Y TIEMPO DE EXPOSICIÓN O ACCIÓN, PUEDEN ALTERAR LA SALUD DE LOS TRABAJADORES, ASÍ COMO LAS FUENTES QUE LOS GENERAN.
-            </td>
+            </th>
+            <th colSpan="3">
+              AGENTES FÍSICOS, QUÍMICOS Y BIOLÓGICOS CAPACES DE MODIFICAR LAS CONDICIONES DEL MEDIO AMBIENTE DEL CENTRO DE TRABAJO QUE, POR SUS PROPIEDADES, CONCENTRACIÓN, NIVEL Y TIEMPO DE EXPOSICIÓN O ACCIÓN, PUEDEN ALTERAR LA SALUD DE LOS TRABAJADORES, ASÍ COMO LAS FUENTES QUE LOS GENERAN.
+            </th>
           </tr>
           <tr>
-            <td className="apply-header">SI</td>
-            <td className="apply-header">NO</td>
+            <th>Área</th>
+            <th>Aplica</th>
+            <th>No Aplica</th>
+            <th>Agentes Químicos</th>
+            <th>Aplica</th>
+            <th>No Aplica</th>
           </tr>
         </thead>
         <tbody>
-          {/* Ejemplo de una fila de datos, deberás replicar esta estructura para cada fila */}
           <tr>
-            <td className="area-side-text" rowSpan="18">MATERIA PRIMA</td>
             <td>Caída de personas a distinto nivel.</td>
-            <td className="apply-cell">x</td>
-            <td className="apply-cell"></td>
-            <td className="agent-header">QUÍMICOS</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td>Vapores orgánicos</td>
+            <td></td>
           </tr>
           <tr>
             <td>Caída de personas al mismo nivel.</td>
-            <td className="apply-cell"></td>
-            <td className="apply-cell">x</td>
-            <td>Vapores orgánicos</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td>Gases</td>
+            <td></td>
           </tr>
           <tr>
-            <td>Caída de objetos en altura.</td>
-            <td className="apply-cell">x</td>
-            <td className="apply-cell"></td>
-            <td>Gases</td>
+            <td>Caída de objetos desprendidos.</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td>Aerosoles</td>
+            <td></td>
           </tr>
-          {/* Continua replicando las filas como se muestra en la imagen */}
+          <tr>
+            <td>Caída de objetos desgajados.</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td>Polvos</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Choque contra objetos inmóviles.</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Choque contra objetos móviles.</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Golpes/cortes por objetos o herramientas.</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Proyección de fragmentos o herramientas.</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Proyección de fluidos o materiales calientes.</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Sobre esfuerzo por carga manual.</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Exposición a temperaturas extremas ambientales.</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td>Ruidos</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Contactos eléctricos: directos e indirectos.</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td>Vibraciones</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Exposición a sustancias químicas tóxicas.</td>
+            <td>X</td>
+            <td></td>
+            <td>Iluminación</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Exposición a radiaciones ionizantes.</td>
+            <td>X</td>
+            <td></td>
+            <td>Radiaciones ionizantes</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Incendios.</td>
+            <td>X</td>
+            <td></td>
+            <td>Radiaciones no ionizantes</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Explosiones.</td>
+            <td>X</td>
+            <td></td>
+            <td>Temperaturas extremas</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Accidentes causados por seres vivos.</td>
+            <td>X</td>
+            <td></td>
+            <td>Agentes biológicos</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Atropellos o golpes de vehículos.</td>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td>Virus</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Bacterias</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Hongos</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Residuos Biológico-Infecciosos</td>
+            <td></td>
+          </tr>
         </tbody>
       </table>
     </div>
   );
 };
 
-export default SafetyTable;
+export default TableComponent;
