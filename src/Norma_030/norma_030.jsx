2b50f11155ac44f3aa7b8de5f64714e750817a26
JuanUtch123 <156106033+JuanUtch123@users.noreply.github.com>
2024-09-17 08:57:48 -0700

    cmodificacion de tabla xd
    xd

--- src/Norma_030/norma_030.jsx
+++ src/Norma_030/norma_030.jsx
@@ -38,30 +38,24 @@ const TableComponent = () => {
             <th rowSpan="29" className="vertical-header">
               CUARTO DE RESIDUOS
             </th>
-            <th colSpan="3">
-              CONDICIONES FÍSICAS PELIGROSAS O INSEGURAS QUE PUEDAN REPRESENTAR UN RIESGO EN LAS INSTALACIONES, PROCESOS, MAQUINARIA, EQUIPO, HERRAMIENTAS, MEDIOS DE TRANSPORTE, MATERIALES Y ENERGÍA.
-            </th>
-            <th colSpan="3">
-              AGENTES FÍSICOS, QUÍMICOS Y BIOLÓGICOS CAPACES DE MODIFICAR LAS CONDICIONES DEL MEDIO AMBIENTE DEL CENTRO DE TRABAJO QUE, POR SUS PROPIEDADES, CONCENTRACIÓN, NIVEL Y TIEMPO DE EXPOSICIÓN O ACCIÓN, PUEDEN ALTERAR LA SALUD DE LOS TRABAJADORES, ASÍ COMO LAS FUENTES QUE LOS GENERAN.
-            </th>
+            
+           
           </tr>
           <tr>
-            <th>Área</th>
+            <th>CONDICIONES FÍSICAS PELIGROSAS O INSEGURAS QUE PUEDAN REPRESENTAR UN RIESGO EN LAS INSTALACIONES, PROCESOS, MAQUINARIA, EQUIPO, HERRAMIENTAS, MEDIOS DE TRANSPORTE, MATERIALES Y ENERGÍA.</th>
             <th>Aplica</th>
             <th>No Aplica</th>
-            <th>Agentes</th>
+            <th>AGENTES FÍSICOS, QUÍMICOS Y BIOLÓGICOS CAPACES DE MODIFICAR LAS CONDICIONES DEL MEDIO AMBIENTE DEL CENTRO DE TRABAJO QUE, POR SUS PROPIEDADES, CONCENTRACIÓN, NIVEL Y TIEMPO DE EXPOSICIÓN O ACCIÓN, PUEDEN ALTERAR LA SALUD DE LOS TRABAJADORES, ASÍ COMO LAS FUENTES QUE LOS GENERAN.</th>
             <th>Aplica</th>
             <th>No Aplica</th>
           </tr>
         </thead>
         <tbody>
           {/* Agentes Químicos */}
-          <tr style={{ color: 'white', fontWeight: 'bold', backgroundColor: 'red' }}>
-            <td colSpan="6">QUÍMICOS</td>
-          </tr>
+          
           <tr>
-            <td>Caída de personas a distinto nivel.</td>
             <td></td>
+            <td>Caída de personas a distinto nivel.</td>
             <td>
             <input
                 type="checkbox"
@@ -81,8 +75,8 @@ const TableComponent = () => {
             <td></td>
           </tr>
           <tr>
-            <td>Caída de personas al mismo nivel.</td>
             <td></td>
+            <td>Caída de personas al mismo nivel.</td>
             <td>
             <input
                 type="checkbox"
@@ -102,8 +96,8 @@ const TableComponent = () => {
             <td></td>
           </tr>
           <tr>
-            <td>Caída de objetos desprendidos.</td>
-            <td>.</td>
+            <td></td>
+            <td>Caída de objetos desprendidos</td>
             
             <td>
             <input
@@ -124,8 +118,8 @@ const TableComponent = () => {
             <td></td>
           </tr>
           <tr>
-            <td>Caída de objetos desgajados.</td>
             <td></td>
+            <td>Caída de objetos desgajados</td>
             <td>
             <input
                 type="checkbox"
@@ -145,13 +139,10 @@ const TableComponent = () => {
             <td></td>
           </tr>
 
-          {/* Agentes Biológicos */}
-          <tr style={{ color: 'white', fontWeight: 'bold', backgroundColor: 'red' }}>
-            <td colSpan="6">BIOLÓGICOS</td>
-          </tr>
+    
           <tr>
-            <td>Choque contra objetos inmóviles.</td>
             <td></td>
+            <td>Choque contra objetos inmóviles.</td>
             <td>
                  <input
                 type="checkbox"
@@ -171,8 +162,8 @@ const TableComponent = () => {
             <td></td>
           </tr>
           <tr>
-            <td>Choque contra objetos móviles.</td>
             <td></td>
+            <td>Choque contra objetos móviles.</td>
             <td>
             <input
                 type="checkbox"
@@ -192,8 +183,8 @@ const TableComponent = () => {
             <td></td>
           </tr>
           <tr>
-            <td>Golpes/cortes por objetos o herramientas.</td>
             <td></td>
+            <td>Golpes/cortes por objetos o herramientas.</td>
             <td>
             <input
                 type="checkbox"
@@ -213,8 +204,8 @@ const TableComponent = () => {
             <td></td>
           </tr>
           <tr>
-            <td>Proyección de fragmentos o herramientas.</td>
             <td></td>
+            <td>Proyección de fragmentos o herramientas.</td>
             <td>
             <input
                 type="checkbox"
@@ -234,13 +225,10 @@ const TableComponent = () => {
             <td></td>
           </tr>
 
-          {/* Agentes Físicos */}
-          <tr style={{ color: 'white', fontWeight: 'bold', backgroundColor: 'red' }}>
-            <td colSpan="6">FÍSICOS</td>
-          </tr>
+    
           <tr>
-            <td>Proyección de fluidos o materiales calientes.</td>
             <td></td>
+            <td>Proyección de fluidos o materiales calientes.</td>
             <td>
             <input
                 type="checkbox"
@@ -260,8 +248,8 @@ const TableComponent = () => {
             <td></td>
           </tr>
           <tr>
-            <td>Sobre esfuerzo por carga manual.</td>
             <td></td>
+            <td>Sobre esfuerzo por carga manual.</td>
             <td>
             <input
                 type="checkbox"
@@ -281,8 +269,8 @@ const TableComponent = () => {
             <td></td>
           </tr>
           <tr>
-            <td>Exposición a temperaturas extremas ambientales.</td>
             <td></td>
+            <td>Exposición a temperaturas extremas ambientales.</td>
             <td>
             <input
                 type="checkbox"
@@ -302,8 +290,8 @@ const TableComponent = () => {
             <td></td>
           </tr>
           <tr>
-            <td>Contactos eléctricos: directos e indirectos.</td>
             <td></td>
+            <td>Contactos eléctricos: directos e indirectos.</td>
             <td>
             <input
                 type="checkbox"
