3dd76a5850a7601e0631cc7b2bb9c3caec2a57bc
JuanUtch123 <156106033+JuanUtch123@users.noreply.github.com>
2024-10-15 11:57:30 -0700

    asisitente avance

--- src/Norma_NOMs/norma_noms.jsx
+++ src/Norma_NOMs/norma_noms.jsx
@@ -3,208 +3,240 @@ import { useNavigate } from 'react-router-dom';
 import './NormaNoms.css';
 
 const NormaNoms = () => {
-  const [selection, setSelection] = useState('0');
-  const [showForm, setShowForm] = useState(false);
-  const [showNextSection, setShowNextSection] = useState(false); 
-  const [answer, setAnswer] = useState('');
-  const [materialesPiroforicos, setMaterialesPiroforicos] = useState(''); // Nuevo estado para materiales pirofóricos
+  const [step, setStep] = useState(1);
+  const [formValues, setFormValues] = useState({
+    superficie: '',
+    invGases: '',
+    invLiquidosi: '',
+    invLiquidosc: '',
+    invSolidos: '',
+    materialesPiroforicos: '',
+    areaTrabajo: '',
+    elementos: [],
+    maquinaria: '',
+    tiposMaquinaria: [],
+    trabajosAltura: '',
+    equiposAltura: [],
+    recipientesCriogenicos: '',
+    categoriasRecipientes: [],
+    generadoresVapor: ''
+  });
+
   const navigate = useNavigate();
 
-  const handleSelectionChange = (e) => {
-    setSelection(e.target.value);
+  const handleNext = () => {
+    setStep(step + 1);
   };
 
-  const handleContinue = () => {
-    if (selection === '0') {
-      setShowForm(true);
+  const handleBack = () => {
+    if (step > 1) {
+      setStep(step - 1);
     } else {
-      navigate('/otro-formulario');
+      navigate(-1);
     }
   };
 
-  const handleAnswerChange = (e) => {
-    setAnswer(e.target.value);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value
+    });
   };
 
-  const handleMaterialesChange = (e) => {
-    setMaterialesPiroforicos(e.target.value); // Guardar respuesta de materiales pirofóricos
+  const handleCheckboxChange = (e, field) => {
+    const { value } = e.target;
+    setFormValues((prev) => ({
+      ...prev,
+      [field]: prev[field].includes(value)
+        ? prev[field].filter((item) => item !== value)
+        : [...prev[field], value]
+    }));
   };
 
-  const handleNextSection = (e) => {
-    e.preventDefault(); // Evitar el comportamiento por defecto del formulario
-    if (materialesPiroforicos === '1') {
-      setShowNextSection(true); // Mostrar la siguiente sección si hay materiales pirofóricos
-    } else {
-      // Si no hay materiales pirofóricos, puedes redirigir a otra parte o hacer otra acción
-      navigate('/otra-seccion'); // Ejemplo de navegación a otra sección
+  const isStepCompleted = () => {
+    switch (step) {
+      case 1:
+        return formValues.area !== '';
+      case 2:
+        return (
+          formValues.superficie &&
+          formValues.invGases &&
+          formValues.invLiquidosi &&
+          formValues.invLiquidosc &&
+          formValues.invSolidos &&
+          formValues.materialesPiroforicos !== ''
+        );
+      case 3:
+        return formValues.areaTrabajo !== '';
+      case 4:
+        return formValues.elementos.length > 0;
+      case 5:
+        return formValues.maquinaria !== '';
+      case 6:
+        return formValues.tiposMaquinaria.length > 0;
+      case 7:
+        return formValues.trabajosAltura !== '';
+      case 8:
+        return formValues.equiposAltura.length > 0;
+      case 9:
+        return formValues.recipientesCriogenicos !== '';
+      case 10:
+        return formValues.categoriasRecipientes.length > 0;
+      case 11:
+        return formValues.generadoresVapor !== '';
+      default:
+        return false;
     }
   };
 
   return (
     <div className="norma-noms-container">
       <div className="container">
-        <h3>Estructura del centro de trabajo</h3>
-        <em>Indique la forma en la cual requiere identificar las NOMs aplicables</em>
+        {/* Paso 1 */}
+        {step === 1 && (
+          <div className="step1">
+            <h3>Estructura del centro de trabajo</h3>
+            <em>Indique la forma en la cual requiere identificar las NOMs aplicables</em>
+            <div className="options">
+              <label>
+                <input type="radio" name="area" value="centro" onChange={handleInputChange} />
+                Para todo el centro de trabajo
+              </label>
+              <label>
+                <input type="radio" name="area" value="proceso" onChange={handleInputChange} />
+                Por área, departamento o proceso
+              </label>
+            </div>
+            <div className="buttons">
+              <button onClick={handleBack}>Regresar</button>
+              <button onClick={handleNext} disabled={!isStepCompleted()}>Continuar</button>
+            </div>
+          </div>
+        )}
 
-        <div className="options">
-          <label>
-            <input
-              type="radio"
-              name="seleccion"
-              value="0"
-              checked={selection === '0'}
-              onChange={handleSelectionChange}
-            />
-            Para todo el centro de trabajo
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="seleccion"
-              value="1"
-              onChange={handleSelectionChange}
-            />
-            Por área, departamento o proceso
-          </label>
-        </div>
-
-        <div className="buttons">
-          <button onClick={() => navigate(-1)}>Regresar</button>
-          <button onClick={handleContinue}>Continuar</button>
-        </div>
-
-        {/* Mostrar el formulario solo si showForm es verdadero */}
-        {showForm && (
-          <div className="fire-risk-assessment">
+        {/* Paso 2 */}
+        {step === 2 && (
+          <div className="step2">
             <h3>Determinación del grado de riesgo de incendio</h3>
-            <br /><br />
-            
-            <table id="ContentPlaceHolder1_UsrRNo" border="0" cellpadding="0" cellspacing="0" style={{ width: '532px' }}>
-              <tr>
-                <td className="tablaresultados1" style={{ width: '248px', height: '12px', textAlign: 'left' }}>
-                  Nombre o razón social:
-                </td>
-                <td style={{ height: '12px', textAlign: 'left', width: '393px' }}></td>
-              </tr>
-            </table>
+            <div className="inventory-fields">
+              <label>
+                Superficie construida:
+                <input
+                  type="number"
+                  name="superficie"
+                  value={formValues.superficie}
+                  onChange={handleInputChange}
+                  required
+                />
+                metros cuadrados
+              </label>
 
-            <table id="Table3" border="0" cellpadding="0" cellspacing="0" width="532">
-              <tr>
-                <td className="tablaresultados1" style={{ width: '207px', height: '12px', textAlign: 'left' }}>
-                  Área, departamento o proceso:
-                </td>
-                <td style={{ height: '12px', textAlign: 'left' }}>
-                  TODO EL CENTRO DE TRABAJO
-                </td>
-              </tr>
-            </table>
-            <br />
+              <label>
+                Inventario de gases inflamables:
+                <input
+                  type="number"
+                  name="invGases"
+                  value={formValues.invGases}
+                  onChange={handleInputChange}
+                  required
+                />
+                litros
+              </label>
 
-            <table align="center" border="0" cellpadding="0" cellspacing="0" width="532">
-              <tr>
-                <th width="522">
-                  <p align="left" style={{ textAlign: 'left' }}>
-                    <img alt="" height="16" src="../App_Themes/Tema1/Imagenes/action_go2.gif" width="16" />
-                    Proporcione la siguiente información:
-                  </p>
-                  <p align="left">&nbsp;</p>
-                </th>
-              </tr>
-            </table>
+              <label>
+                Inventario de líquidos inflamables:
+                <input
+                  type="number"
+                  name="invLiquidosi"
+                  value={formValues.invLiquidosi}
+                  onChange={handleInputChange}
+                  required
+                />
+                litros
+              </label>
 
-            <table align="center" border="0" cellpadding="0" cellspacing="10" width="532">
-              <tr>
-                <td style={{ textAlign: 'left' }}>Superficie construida</td>
-                <td style={{ textAlign: 'left' }}>
-                  <input name="ctl00$ContentPlaceHolder1$superficie" type="text" id="ContentPlaceHolder1_superficie" /><br />
-                  <span id="ContentPlaceHolder1_RequiredFieldValidator3" style={{ display: 'none' }}>Campo requerido</span>
-                </td>
-                <td style={{ textAlign: 'left' }}>metros cuadrados</td>
-              </tr>
-              <tr>
-                <td style={{ textAlign: 'left' }}>Inventario de gases inflamables</td>
-                <td style={{ textAlign: 'left' }}>
-                  <input name="ctl00$ContentPlaceHolder1$invGases" type="text" id="ContentPlaceHolder1_invGases" /><br />
-                  <span id="ContentPlaceHolder1_RequiredFieldValidator4" style={{ display: 'none' }}>Campo requerido</span>
-                </td>
-                <td style={{ textAlign: 'left' }}>litros</td>
-              </tr>
-              <tr>
-                <td style={{ textAlign: 'left' }}>Inventario de líquidos inflamables</td>
-                <td style={{ textAlign: 'left' }}>
-                  <input name="ctl00$ContentPlaceHolder1$invLiquidosi" type="text" id="ContentPlaceHolder1_invLiquidosi" /><br />
-                  <span id="ContentPlaceHolder1_RequiredFieldValidator5" style={{ display: 'none' }}>Campo requerido</span>
-                </td>
-                <td style={{ textAlign: 'left' }}>litros</td>
-              </tr>
-              <tr>
-                <td style={{ textAlign: 'left' }}>Inventario de líquidos combustibles</td>
-                <td style={{ textAlign: 'left' }}>
-                  <input name="ctl00$ContentPlaceHolder1$invLiquidosc" type="text" id="ContentPlaceHolder1_invLiquidosc" /><br />
-                  <span id="ContentPlaceHolder1_RequiredFieldValidator6" style={{ display: 'none' }}>Campo requerido</span>
-                </td>
-                <td style={{ textAlign: 'left' }}>litros</td>
-              </tr>
-              <tr>
-                <td style={{ textAlign: 'left' }}>Inventario de sólidos combustibles, incluido el mobiliario del centro de trabajo</td>
-                <td style={{ textAlign: 'left' }}>
-                  <input name="ctl00$ContentPlaceHolder1$invSolidos" type="text" id="ContentPlaceHolder1_invSolidos" /><br />
-                  <span id="ContentPlaceHolder1_RequiredFieldValidator7" style={{ display: 'none' }}>Campo requerido</span>
-                </td>
-                <td style={{ textAlign: 'left' }}>kilogramos</td>
-              </tr>
-            </table>
+              <label>
+                Inventario de líquidos combustibles:
+                <input
+                  type="number"
+                  name="invLiquidosc"
+                  value={formValues.invLiquidosc}
+                  onChange={handleInputChange}
+                  required
+                />
+                litros
+              </label>
 
-            {/* Nueva sección para la pregunta de materiales pirofóricos o explosivos */}
-            <table align="center" border="0" cellpadding="0" cellspacing="0" width="532">
-              <tr>
-                <th width="522">
-                  <p align="left" style={{ textAlign: 'left' }}>
-                    <img alt="" height="16" src="../App_Themes/Tema1/Imagenes/action_go2.gif" width="16" />
-                    ¿Tiene inventario de materiales pirofóricos o explosivos?
-                  </p>
-                  <p align="left" style={{ textAlign: 'left' }}>&nbsp;</p>
-                </th>
-              </tr>
-            </table>
+              <label>
+                Inventario de sólidos combustibles, incluido el mobiliario del centro de trabajo:
+                <input
+                  type="number"
+                  name="invSolidos"
+                  value={formValues.invSolidos}
+                  onChange={handleInputChange}
+                  required
+                />
+                kilogramos
+              </label>
+            </div>
 
-            <table align="center" border="0" cellpadding="0" cellspacing="10" width="532">
-              <tr>
-                <td align="right" colspan="3" style={{ textAlign: 'left' }}>
-                  <table id="ContentPlaceHolder1_tieneMateriales" style={{ width: '150px' }}>
-                    <tr>
-                      <td>
-                        <input
-                          id="ContentPlaceHolder1_tieneMateriales_0"
-                          type="radio"
-                          name="ctl00$ContentPlaceHolder1$tieneMateriales"
-                          value="1"
-                          onChange={handleMaterialesChange}
-                        />
-                        <label htmlFor="ContentPlaceHolder1_tieneMateriales_0">Sí</label>
-                      </td>
-                      <td>
-                        <input
-                          id="ContentPlaceHolder1_tieneMateriales_1"
-                          type="radio"
-                          name="ctl00$ContentPlaceHolder1$tieneMateriales"
-                          value="0"
-                          onChange={handleMaterialesChange}
-                        />
-                        <label htmlFor="ContentPlaceHolder1_tieneMateriales_1">No</label>
-                      </td>
-                    </tr>
-                  </table>
-                </td>
-              </tr>
-            </table>
+            <label>¿Tiene inventario de materiales pirofóricos o explosivos?</label>
+            <label>
+              <input type="radio" name="materialesPiroforicos" value="sí" onChange={handleInputChange} />
+              Sí
+            </label>
+            <label>
+              <input type="radio" name="materialesPiroforicos" value="no" onChange={handleInputChange} />
+              No
+            </label>
 
-            {/* Botón para avanzar a la siguiente sección */}
-            {showNextSection && (
-              <div className="buttons">
-                <button onClick={handleNextSection}>Siguiente</button>
-              </div>
-            )}
+            <div className="buttons">
+              <button onClick={handleBack}>Regresar</button>
+              <button onClick={handleNext} disabled={!isStepCompleted()}>Continuar</button>
+            </div>
+          </div>
+        )}
+
+        {/* Repite la misma estructura para los demás pasos */}
+        {/* Paso 3 */}
+        {step === 3 && (
+          <div className="step3">
+            <h3>Área de trabajo</h3>
+            <label>¿Desarrolla sus actividades en áreas exteriores, como estacionamientos?</label>
+            <label>
+              <input type="radio" name="areaTrabajo" value="sí" onChange={handleInputChange} />
+              Sí
+            </label>
+            <label>
+              <input type="radio" name="areaTrabajo" value="no" onChange={handleInputChange} />
+              No
+            </label>
+            <div className="buttons">
+              <button onClick={handleBack}>Regresar</button>
+              <button onClick={handleNext} disabled={!isStepCompleted()}>Continuar</button>
+            </div>
+          </div>
+        )}
+
+        {/* ... otros pasos se estructuran de forma similar ... */}
+
+        {/* Paso 11 */}
+        {step === 11 && (
+          <div className="step11">
+            <h3>¿En su centro de trabajo están instalados generadores de vapor o calderas?</h3>
+            <label>
+              <input type="radio" name="generadoresVapor" value="sí" onChange={handleInputChange} />
+              Sí
+            </label>
+            <label>
+              <input type="radio" name="generadoresVapor" value="no" onChange={handleInputChange} />
+              No
+            </label>
+            <div className="buttons">
+              <button onClick={handleBack}>Regresar</button>
+              <button onClick={() => navigate('/final')} disabled={!isStepCompleted()}>Finalizar</button>
+            </div>
           </div>
         )}
       </div>
