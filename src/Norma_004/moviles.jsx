7a254bfe57cc2401a03f0cc3b6163a484460aa2f
fabiant <erickthf@gmail.com>
2024-10-15 12:53:28 -0600

    modificaciones en la tabla de moviles (hdspm ta bien hardcore))

--- src/Norma_004/moviles.jsx
+++ src/Norma_004/moviles.jsx
@@ -2,15 +2,11 @@ import React, { useState } from 'react';
 import './Moviles.css';
 
 const RiskTable = () => {
-  // Estados iniciales para Consecuencia, Exposición y Probabilidad
   const [consequence, setConsequence] = useState('Lesiones sin baja');
   const [exposure, setExposure] = useState('Ocasionalmente');
   const [probability, setProbability] = useState('Coincidencia extremadamente remota pero concebible');
-
-  // Estado para manejar la imagen cargada
   const [image, setImage] = useState(null);
 
-  // Función para manejar la carga de imagen
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -19,47 +15,48 @@ const RiskTable = () => {
     }
   };
 
-  // Opciones de identificación de riesgos
   const riskOptions = [
-    "Riesgo caída de piedra o golpe por objeto",
     "Riesgo de incendio",
-    "Riesgo de atrapamiento o volcadura",
     "Riesgo de choque o atropello",
-    "Riesgo de descarga eléctrica",
-    "Exposición a temperaturas elevadas y abatidas",
     "Vibración",
-    "Riesgo inundación",
-    "Ventilación deficiente",
-    "Daños ergonómicos",
-    "Sustancias químicas",
-    "Poca iluminación",
     "Partículas suspendidas en el ambiente",
+    "Riesgo de atrapamiento o volcadura",
   ];
 
-  // Opciones de sistemas de seguridad
   const securityOptions = [
-    "Paro de emergencia",
-    "Sistema Ansul",
     "Extintores",
+    "Guardas, parachoques y protectores de calaveras",
     "Freno de emergencia",
-    "Bandas antiestáticas",
-    "Sistema de anclaje",
-    "Programas de mantenimiento",
-    "Guardaparachoques y protectores de calaveras",
-    "Calza de seguridad",
-    "Bloqueo de sistema hidráulico",
-    "Indicadores (torreta, luces, claxon, banderas, etc)",
-    "Bloqueo de sistema eléctrico",
-    "Cinturón de seguridad",
     "Barandales y escaleras",
-    "Indicadores (agua, aceite, aire, motor, presión, etc)",
-    "Canopy",
-    "Mata chispas",
-    "Sistema LOTO",
-    "Guardas"
+    "Indicadores (torreta, luces, claxon, banderas, etc)",
   ];
 
-  // Funciones para calcular los valores
+  const opcionesConsecuencia = [
+    'Catástrofe',
+    'Varias muertes',
+    'Muerte',
+    'Lesiones graves',
+    'Lesiones con baja',
+    'Lesiones sin baja'
+  ];
+
+  const opcionesExposicion = [
+    'Continuamente',
+    'Frecuentemente',
+    'Ocasionalmente',
+    'Irregularmente',
+    'Raramente'
+  ];
+
+  const opcionesProbabilidad = [
+    'Es el resultado más probable y esperado',
+    'Es completamente posible, no será nada extraño',
+    'Sería una secuencia o coincidencia rara pero posible, ha ocurrido',
+    'Coincidencia muy rara, pero se sabe que ha ocurrido',
+    'Coincidencia extremadamente remota pero concebible',
+    'Coincidencia prácticamente imposible, jamás ha ocurrido'
+  ];
+
   const calcularValorConsecuencia = (consequence) => {
     const valoresConsecuencia = {
       'Catástrofe': 50,
@@ -95,7 +92,6 @@ const RiskTable = () => {
     return valoresProbabilidad[probability] || 0;
   };
 
-  // Cálculo de la magnitud del riesgo
   const calcularMagnitudRiesgo = () => {
     const valorConsecuencia = calcularValorConsecuencia(consequence);
     const valorExposicion = calcularValorExposicion(exposure);
@@ -103,7 +99,6 @@ const RiskTable = () => {
     return Math.floor(valorConsecuencia * valorExposicion * valorProbabilidad);
   };
 
-  // Clasificación de la magnitud del riesgo
   const obtenerClasificacionRiesgo = (magnitud) => {
     if (magnitud > 400) {
       return { color: 'red', texto: 'Muy Alto: Detención inmediata', accion: 'Inmediata', clasificacion: 'Muy Alto' };
@@ -121,195 +116,129 @@ const RiskTable = () => {
   const magnitudRiesgo = calcularMagnitudRiesgo();
   const { color, texto, accion, clasificacion } = obtenerClasificacionRiesgo(magnitudRiesgo);
 
-  // Opciones para los selects de Consecuencia, Exposición y Probabilidad
-  const opcionesConsecuencia = [
-    'Catástrofe',
-    'Varias muertes',
-    'Muerte',
-    'Lesiones graves',
-    'Lesiones con baja',
-    'Lesiones sin baja'
-  ];
-
-  const opcionesExposicion = [
-    'Continuamente',
-    'Frecuentemente',
-    'Ocasionalmente',
-    'Irregularmente',
-    'Raramente'
-  ];
-
-  const opcionesProbabilidad = [
-    'Es el resultado más probable y esperado',
-    'Es completamente posible, no será nada extraño',
-    'Sería una secuencia o coincidencia rara pero posible, ha ocurrido',
-    'Coincidencia muy rara, pero se sabe que ha ocurrido',
-    'Coincidencia extremadamente remota pero concebible',
-    'Coincidencia prácticamente imposible, jamás ha ocurrido'
-  ];
-
   return (
-    <table className="risk-table">
-      <thead>
-        <tr>
-          <th colSpan="5" className="red">Nombre de la maquinaria o equipo:</th>
-          <th>MONTACARGAS EXTERIOR MINA</th>
-          <th className="red">Área:</th>
-          <th>EXTERIOR MINA</th>
-        </tr>
-        <tr>
-          <th colSpan="5" className="red">Descripción de la maquinaria o equipo:</th>
-          <th>LEVANTAMIENTO Y TRANSPORTE DE MATERIAL</th>
-          <th className="red">Energía utilizada:</th>
-          <th>DIESEL</th>
-        </tr>
-        <tr>
-          <th colSpan="2" className="red">Localización esquemática de los riesgos en maquinaria y/o equipo</th>
-          <th className="red">POE:</th>
-          <th>1</th>
-          <th className="red">Tiempo de exposición:</th>
-          <th>12 HRS</th>
-          <th className="red">Fecha de inspección:</th>
-          <th>19/07/2024</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          {/* Primera columna: Imagen */}
-          <td colSpan="3" rowSpan="3" className="image-section">
-            <input type="file" accept="image/*" onChange={handleImageUpload} />
-            {image ? (
-              <img src={image} alt="Uploaded Equipment" className="equipment-image" />
-            ) : (
-              <p>No image uploaded.</p>
-            )}
-          </td>
-          {/* Segunda columna: Identificación de riesgos */}
-          <td colSpan="5" rowSpan="1" className="risk-info">
-            <table className="inner-table">
-              <thead>
-                <tr>
-                  <th className="red">Identificación de riesgos</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* Mostrar solo 7 menús desplegables */}
+    <div className="risk-table-container">
+      <table className="risk-table">
+        <thead>
+          <tr>
+            <th className="red">Nombre de la maquinaria o equipo:</th>
+            <td><input type="text" /></td>
+            <th className="red">Área:</th>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <th className="red">Descripción de la maquinaria o equipo:</th>
+            <td><input type="text" /></td>
+            <th className="red">Energía utilizada:</th>
+            <td>
+              <select>
+                <option value="">Seleccione tipo de energía</option>
+                <option value="eléctrica">Eléctrica</option>
+                <option value="manual">Manual</option>
+                <option value="mecánica">Mecánica</option>
+                <option value="hidráulica">Hidráulica</option>
+                <option value="eólica">Eólica</option>
+                <option value="térmica por combustión">Térmica por combustión</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <th className="red">Fecha de inspección:</th>
+            <td><input type="date" /></td>
+            <th className="red">Localización de riesgos:</th>
+            <td>
+              <input type="file" accept="image/*" onChange={handleImageUpload} />
+              {image && <img src={image} alt="Uploaded Equipment" className="equipment-image" />}
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="risk-info" colSpan="7">
+              <h4 className="red">Identificación de riesgos</h4>
+              <div className="risk-selects">
                 {[...Array(7)].map((_, index) => (
-                  <tr key={index}>
+                  <select key={index}>
+                    <option value="">Seleccione un riesgo</option>
+                    {riskOptions.map((opt, idx) => (
+                      <option key={idx} value={opt}>{opt}</option>
+                    ))}
+                  </select>
+                ))}
+              </div>
+            </td>
+            <td className="safety-info" colSpan="4">
+              <h4 className="red">Sistemas de seguridad</h4>
+              <div className="safety-selects">
+                {[...Array(7)].map((_, index) => (
+                  <select key={index}>
+                    <option value="">Seleccione un sistema de seguridad</option>
+                    {securityOptions.map((opt, idx) => (
+                      <option key={idx} value={opt}>{opt}</option>
+                    ))}
+                  </select>
+                ))}
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="8" className="risk-evaluation-section">
+              <table>
+                <thead>
+                  <tr className="red">
+                    <th colSpan="3">Evaluación de riesgo de trabajo</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Consecuencia</td>
+                    <td>Exposición</td>
+                    <td>Probabilidad</td>
+                  </tr>
+                  <tr>
                     <td>
-                      <select>
-                        <option value="">Seleccione un riesgo</option>
-                        {riskOptions.map((opt, idx) => (
-                          <option key={idx} value={opt}>
-                            {opt}
-                          </option>
+                      <select value={consequence} onChange={(e) => setConsequence(e.target.value)}>
+                        {opcionesConsecuencia.map((opcion, idx) => (
+                          <option key={idx} value={opcion}>{opcion}</option>
+                        ))}
+                      </select>
+                    </td>
+                    <td>
+                      <select value={exposure} onChange={(e) => setExposure(e.target.value)}>
+                        {opcionesExposicion.map((opcion, idx) => (
+                          <option key={idx} value={opcion}>{opcion}</option>
+                        ))}
+                      </select>
+                    </td>
+                    <td>
+                      <select value={probability} onChange={(e) => setProbability(e.target.value)}>
+                        {opcionesProbabilidad.map((opcion, idx) => (
+                          <option key={idx} value={opcion}>{opcion}</option>
                         ))}
                       </select>
                     </td>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </td>
-          {/* Tercera columna: Sistemas de seguridad */}
-          <td colSpan="4" className="safety-info">
-            <table className="inner-table">
-              <thead>
-                <tr>
-                  <th className="red">Sistemas de seguridad</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* Mostrar 7 menús desplegables para sistemas de seguridad */}
-                {[...Array(7)].map((_, index) => (
-                  <tr key={index}>
-                    <td>
-                      <select>
-                        <option value="">Seleccione un sistema de seguridad</option>
-                        {securityOptions.map((opt, idx) => (
-                          <option key={idx} value={opt}>
-                            {opt}
-                          </option>
-                        ))}
-                      </select>
-                    </td>
+                </tbody>
+              </table>
+              <table className="risk-classification">
+                <thead>
+                  <tr className="red">
+                    <th colSpan="3">Clasificación de Magnitud de Riesgo</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </td>
-        </tr>
-        {/* Sección de Evaluación de riesgos y EPP sugerido */}
-        <tr>
-          {/* Evaluación de riesgos */}
-          <td colSpan="6" className="risk-evaluation-section">
-            <table>
-              <thead>
-                <tr className="red">
-                  <th colSpan="3">Evaluación de riesgo de trabajo</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Consecuencia</td>
-                  <td>Exposición</td>
-                  <td>Probabilidad</td>
-                </tr>
-                <tr>
-                  <td>
-                    <select value={consequence} onChange={(e) => setConsequence(e.target.value)}>
-                      {opcionesConsecuencia.map((opcion, idx) => (
-                        <option key={idx} value={opcion}>{opcion}</option>
-                      ))}
-                    </select>
-                  </td>
-                  <td>
-                    <select value={exposure} onChange={(e) => setExposure(e.target.value)}>
-                      {opcionesExposicion.map((opcion, idx) => (
-                        <option key={idx} value={opcion}>{opcion}</option>
-                      ))}
-                    </select>
-                  </td>
-                  <td>
-                    <select value={probability} onChange={(e) => setProbability(e.target.value)}>
-                      {opcionesProbabilidad.map((opcion, idx) => (
-                        <option key={idx} value={opcion}>{opcion}</option>
-                      ))}
-                    </select>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <table className="risk-classification">
-              <thead>
-                <tr className="red">
-                  <th colSpan="4">Clasificación de Magnitud de Riesgo</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr style={{ backgroundColor: color }}>
-                  <td>Magnitud del Riesgo: {magnitudRiesgo}</td>
-                  <td>Clasificación: {clasificacion}</td>
-                  <td>Acción: {accion}</td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
-          {/* EPP sugerido */}
-          <td colSpan="3" className="epp-section">
-            <div className="table-cell red">EPP sugerido</div>
-            <div className="epp-icons">
-              <img src="path-to-icon1.png" alt="EPP Icon 1" />
-              <img src="path-to-icon2.png" alt="EPP Icon 2" />
-              <img src="path-to-icon3.png" alt="EPP Icon 3" />
-              <img src="path-to-icon4.png" alt="EPP Icon 4" />
-              <img src="path-to-icon5.png" alt="EPP Icon 5" />
-              <img src="path-to-icon6.png" alt="EPP Icon 6" />
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+                </thead>
+                <tbody>
+                  <tr style={{ backgroundColor: color }}>
+                    <td>Magnitud del Riesgo: {magnitudRiesgo}</td>
+                    <td>Clasificación: {clasificacion}</td>
+                    <td>Acción: {accion}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
